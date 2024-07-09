8245c6898fa0c7e3ecad02bdc4de47825cc8bec9
15rohan <rohan141166@gmail.com>
2024-07-09 19:47:26 +0530

    Register API

---
+++ roomie-backend/middleware/error-handler.js
@@ -0,0 +1,23 @@
+const { StatusCodes } = require('http-status-codes')
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.msg || "Something went wrong, try again later"
+    }
+
+    if(err.name === 'ValidationError'){
+        customError.msg = Object.values(err.errors)
+        .map((item)=> item.message)
+        .join(',')
+    }
+
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate values for ${Object.keys(err.keyValue)} field. Please provide another value.`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    return res.status(customError.statusCode).json({ msg: customError.msg })
+}
+
+module.exports = errorHandlerMiddleware
--- roomie-backend/server.js
+++ roomie-backend/server.js
@@ -1,17 +1,43 @@
 require('dotenv').config()
 require('express-async-errors')
 
+//security packages
+const cors = require('cors')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express')
 const app = express()
 
 const connectDB = require('./db/connect')
 
+//routers
 const authRouter = require('./routes/auth');
 
+//error handlers
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//security
+app.use(
+    rateLimiter({
+        windowsMs: 15*60*1000,
+        max: 100
+    })
+)
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 //routes
 app.use('/api/v1/auth',authRouter)
+
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 8080;
 
