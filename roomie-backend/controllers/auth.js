f0a51215f9647fc9ba028b215ca62ec066a18af6
15rohan <rohan141166@gmail.com>
2024-07-10 03:37:14 +0530

    Login API

--- roomie-backend/controllers/auth.js
+++ roomie-backend/controllers/auth.js
@@ -1,16 +1,33 @@
 const User = require('../models/user')
 const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body })
-    const token = user.createJWT();
+    const token = user.createJWT()
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
-    const {email,password} = req.body;
-    res.status(200).send('Login User')
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    //compare pass
+    const correctPass = await user.comparePassword(password)
+    if (!correctPass) {
+        throw new UnauthenticatedError('Invalid Password')
+    }
+    const token = user.createJWT()
+    res.status(200).json({ user: { name: user.name }, token })
 }
 
 module.exports = { register, login }
