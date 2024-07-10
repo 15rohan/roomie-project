52cc711268bdc692ee608b3e54e2c69c4cc6787f
Rahul <imrahul2308@gmail.com>
2024-07-11 05:05:57 +0530

    registration popup

---
+++ roomie-frontend/src/components/RegisterOverlay.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import { useFormik } from "formik";
+import * as yup from 'yup'
+import { axiosInstance } from '../service/axios'
+
+const RegisterOverlay = (props) => {
+
+    const [checked,setChecked]=React.useState({
+        male:false,
+        female:false
+    })
+
+    const [status,setStatus]=React.useState('')
+
+    const schema = yup.object().shape({
+        name: yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+        email: yup.string().email('Invalid email').required('Required'),
+        college: yup.string().required('Required'),
+        age: yup.number().required('Required'),
+        password: yup.string().required('Required'),
+        gender: yup.string().required('Required')
+    })
+
+    const registerUser = async (data) => {
+        try {
+            const user = {
+                name: data.name,
+                college_name: data.college,
+                email: data.email,
+                age: data.age,
+                password: data.password,
+                gender: data.gender
+            }
+            const result = await axiosInstance.post('api/v1/auth/register', user)
+            console.log(result, 'result')
+                setStatus('Registration Successful!')
+            
+        } catch (error) {
+            console.log(error)
+            setStatus('Could not register,please try again')
+        }
+    }
+
+    const { values, handleChange, handleSubmit, touched, errors, resetForm } = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            college: '',
+            age: '',
+            password: '',
+            gender: ''
+        },
+        validationSchema: schema,
+        onSubmit: (values) => {
+            registerUser(values)
+            resetForm()
+            setChecked({
+                male:false,
+                female:false
+            })
+        }
+    })
+
+    console.log(values)
+
+    return (
+        <div className="home w-full absolute z-10 bg-black bg-opacity-40 h-full">
+            <div className="registration bg-[#2B2BD7]">
+                <div className="text-3xl text-white font-semibold text-center">
+                    <h1>Register Here!</h1>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form">
+                        <input type="text" name='name' value={values.name} onChange={handleChange} placeholder='Enter Your Name' />
+                        <input type="text" name='email' value={values.email} onChange={handleChange} placeholder='Enter Your Email' />
+                        <input type="password" name='password' value={values.password} onChange={handleChange} placeholder='Set Your Password' />
+                        <div className='flex gap-2'>
+                            <input type="number" className='flex-1' name="age" id="age" value={values.age} onChange={handleChange} placeholder='Enter Your Age' />
+                            <select required className='flex-1' name="college" id="college" value={values.college} onChange={handleChange} >
+                                <option value="" selected={true} disabled>Select Your College</option>
+                                <option value="VIT">VIT</option>
+                                <option value="SRM">SRM</option>
+                                <option value="BITS">BITS</option>
+                            </select>
+                        </div>
+                        <div className='flex flex-col gap-1 text-white'>
+                            <div className='flex items-center gap-2'>
+                                <input type="radio" className='w-3 h-3' name="gender" value='Male' id="male" onChange={handleChange} />
+                                <label htmlFor="male">Male</label>
+                            </div>
+                            <div className='flex items-center gap-2'>
+                                <input type="radio" className='w-3 h-3' name="gender" value='Female' id="female" onChange={handleChange} />
+                                <label htmlFor="female">Female</label>
+                            </div>
+                        </div>
+                    {status && <p className='text-red-500 text-lg font-semibold'>{status}</p>}
+                        <div className="submit">
+                            <button type='submit'>Submit</button>
+                            <div className='button-clear bg-[#8DD1E7]' onClick={resetForm}>Clear</div>
+                            <div className='button-clear bg-white' onClick={()=>props.togglePopup()}>Close</div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterOverlay
--- roomie-frontend/src/pages/Home.jsx
+++ roomie-frontend/src/pages/Home.jsx
@@ -1,94 +1,26 @@
 import React from 'react'
-import { useFormik } from "formik";
-import * as yup from 'yup'
-import { axiosInstance } from '../service/axios'
+import RegisterOverlay from '../components/RegisterOverlay'
 
 const Home = () => {
 
-    const schema = yup.object().shape({
-        name: yup.string()
-            .min(2, 'Too Short!')
-            .matches(/^[a-zA-Z0-9]+$/, 'Invalid')
-            .max(50, 'Too Long!')
-            .required('Required'),
-        email: yup.string().email('Invalid email').required('Required'),
-        college: yup.string().required('Required'),
-        age: yup.number().required('Required'),
-        password: yup.string().required('Required'),
-        gender: yup.string().required('Required')
-    })
+    const [popup, setPopup] = React.useState(false)
+    // console.log(popup)
 
-    const registerUser = async (data) => {
-        try {
-            const user = {
-                name: data.name,
-                college_name: data.college,
-                email: data.email,
-                age: data.age,
-                password: data.password,
-                gender: data.gender
-            }
-            const result = await axiosInstance.post('api/v1/auth/register', user)
-            console.log(result, 'result')
-        } catch (error) {
-            console.log(error)
-        }
+    const togglePopup=()=>{
+        setPopup((prev)=>!prev)
     }
 
-    const { values, handleChange, handleSubmit, touched, errors, resetForm } = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            college: '',
-            age: '',
-            password: '',
-            gender: ''
-        },
-        validationSchema: schema,
-        onSubmit: (values) => {
-            registerUser(values)
-        }
-    })
-
-    // console.log(values, errors)
-
     return (
-        <div className="home">
-            <div className="registration">
-                <div className="text-3xl text-white font-semibold text-center">
-                    <h1>Register Here!</h1>
+        <div className="relative h-screen">
+            {popup && <RegisterOverlay togglePopup={togglePopup}/>}
+            <nav className='w-full h-14 bg-blue-500 flex items-center justify-between text-white px-5'>
+                <div><p className='text-2xl font-bold cursor-pointer'>Roomie</p></div>
+                <div className='flex gap-6 items-center justify-center'>
+                <li className='list-none cursor-pointer hover:text-slate-200 text-md'>Home</li>
+                <li className='list-none cursor-pointer hover:text-slate-200 text-md'>Login</li>
+                <li className='list-none cursor-pointer hover:text-slate-200 text-md' onClick={()=>togglePopup()}>Register</li>
                 </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="form">
-                        <input type="text" name='name' value={values.name} onChange={handleChange} placeholder='Enter Your Name' />
-                        <input type="text" name='email' value={values.email} onChange={handleChange} placeholder='Enter Your Email' />
-                        <input type="password" name='password' value={values.password} onChange={handleChange} placeholder='Set Your Password' />
-                        <div className='flex gap-2'>
-                            <input type="number" className='flex-1' name="age" id="age" value={values.age} onChange={handleChange} placeholder='Enter Your Age' />
-                            <select required className='flex-1' name="college" id="college" value={values.college} onChange={handleChange} >
-                                <option value="" selected={true} disabled>Select Your College</option>
-                                <option value="VIT">VIT</option>
-                                <option value="SRM">SRM</option>
-                                <option value="BITS">BITS</option>
-                            </select>
-                        </div>
-                        <div className='flex flex-col gap-1 text-white'>
-                            <div className='flex items-center gap-2'>
-                                <input type="radio" className='w-3 h-3' name="gender" value='Male' id="male" onChange={handleChange} />
-                                <label htmlFor="male">Male</label>
-                            </div>
-                            <div className='flex items-center gap-2'>
-                                <input type="radio" className='w-3 h-3' name="gender" value='Female' id="female" onChange={handleChange} />
-                                <label htmlFor="female">Female</label>
-                            </div>
-                        </div>
-                        <div className="submit">
-                            <button type='submit'>Submit</button>
-                            <div className='button-clear' onClick={resetForm}>Clear</div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            </nav>
         </div>
     )
 }
