ae8ade78cf4d3efa4e05a38b307dfc51ff23017c
15rohan <rohan141166@gmail.com>
2024-07-12 04:54:16 +0530

    updated error message

--- roomie-backend/controllers/auth.js
+++ roomie-backend/controllers/auth.js
@@ -4,6 +4,9 @@ const { BadRequestError, UnauthenticatedError } = require('../errors')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
+    if(req.body.password.length < 8){
+        throw new BadRequestError('Password must be atleast 8 characters')
+    }
     const user = await User.create({ ...req.body })
     const token = user.createJWT()
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
